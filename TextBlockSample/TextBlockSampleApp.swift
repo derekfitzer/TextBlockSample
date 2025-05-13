03890a212d310b91599a19a1f99757daebaaa3ed
Derek Fitzer <franzderek@gmail.com>
2025-05-13 07:17:39 -0400

    Initial Commit

---
+++ TextBlockSample/TextBlockSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextBlockSampleApp.swift
+//  TextBlockSample
+//
+//  Created by Derek Fitzer on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TextBlockSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
